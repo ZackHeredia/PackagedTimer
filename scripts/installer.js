f8713e520d61b898052685b5fe4369aa80562df3
ZackHeredia <ZacarysHeredia@gmail.com>
2015-01-02 18:00:56 -0400

    onsucces and onerror callback replaced

--- scripts/installer.js
+++ scripts/installer.js
@@ -2,33 +2,16 @@ $(function ()
 {
 	var manifestUrl = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
 	var request = navigator.mozApps.checkInstalled(manifestUrl);
-	alert(manifestUrl);
-
-/*	request.onsuccess = function()
-	{
-		alert("suces");
-		if (request.result)
-			
-		else
-		{
-			
-		}
-	}
-	request.onerror = function()
-	{
 	
-	}*/
-
 	if (request.error)
 		$("#wrap").append("<p>Ocurrio un error verificando la instalacion: " + request.error.message + "</p>");
 	else if (request.result)
-		$("#wrap").append("<p>Esta app Ya esta instalada en su dispositivo</p>");
+		$("#wrap").append("<p>Esta app ya esta instalada en su dispositivo</p>");
 	else
 	{
 		$("#btnInstall").click(function()
 		{
 			var req = navigator.mozApps.install(manifestUrl);
-			alert(req + "intall " + manifestUrl);
 
 			req.onsuccess = function()
 			{
