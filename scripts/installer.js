ed1fe7988a85566d48b86b9ebcedccd53acad1b9
ZackHeredia <ZacarysHeredia@gmail.com>
2015-01-02 05:07:51 -0400

    Another manifest url added

--- scripts/installer.js
+++ scripts/installer.js
@@ -1,18 +1,18 @@
 $(function ()
 {
-	var manifestUrl = location.href.replace("index.html", "manifest.webapp");
+	var manifestUrl = "http://zackheredia.github.io/HostedTimer/manifest.webapp";
 	var request = navigator.mozApps.checkInstalled(manifestUrl);
 
-	$("#btnInstall").click(function ()
+	$("#btnInstall").click(function()
 	{
-		request.onsuccess = function ()
+		request.onsuccess = function()
 		{
 			if (request.result)
 				$("#wrap").append("<p>Esta app Ya esta instalada en su dispositivo</p>");
 			else
 				install(manifestUrl);
 		}
-		request.onerror = function ()
+		request.onerror = function()
 		{
 			$("#wrap").append("<p>Ocurrio un error verificando la instalacion: " + this.error.message + "</p>");
 		}
@@ -28,13 +28,13 @@ function install (manifestUrl)
 {
 	var request = navigator.mozApps.install(manifestUrl);
 
-	request.onsuccess = function ()
+	request.onsuccess = function()
 	{
 		$("#btnInstall").unbind("click");
-		$("#wrap").append("<p>La app se ha instalado en su dispositivo</p>")
+		$("#wrap").append("<p>La app se ha instalado en su dispositivo</p>");
 	}
-	request.onerror = function ()
+	request.onerror = function()
 	{
-		$("#wrap").append("<p>Ocurrio un error durante la instalacion: " + this.error.name + "</p>")
+		$("#wrap").append("<p>Ocurrio un error durante la instalacion: " + this.error.name + "</p>");
 	}
 }
