5ac97b312e9360807bc9b5af59c3ed51a0a17255
ZackHeredia <ZacarysHeredia@gmail.com>
2015-01-02 12:05:17 -0400

    Update installer.js

--- scripts/installer.js
+++ scripts/installer.js
@@ -9,21 +9,22 @@ $(function ()
 	alert(request.onsuccess);
 	request.onsuccess = function()
 	{
+		alert("suces");
 		if (request.result)
 			$("#wrap").append("<p>Esta app Ya esta instalada en su dispositivo</p>");
 		else
 		{
 			$("#btnInstall").click(function()
 			{
-				var request = navigator.mozApps.install(manifestUrl);
-				alert(request + "intall " + manifestUrl);
+				var req = navigator.mozApps.install(manifestUrl);
+				alert(req + "intall " + manifestUrl);
 
-				request.onsuccess = function()
+				req.onsuccess = function()
 				{
 					$("#wrap").append("<p>La app se ha instalado en su dispositivo</p>");
 					$("#btnInstall").unbind("click");
 				}
-				request.onerror = function()
+				req.onerror = function()
 				{
 					$("#wrap").append("<p>Ocurrio un error durante la instalacion: " + this.error.name + "</p>");
 				}
