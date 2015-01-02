14e822e2ddd34fe122f42406bc6827ff096abd71
ZackHeredia <ZacarysHeredia@gmail.com>
2015-01-02 12:01:42 -0400

    Update installer.js

--- scripts/installer.js
+++ scripts/installer.js
@@ -6,40 +6,37 @@ $(function ()
 	alert(request.result+" result "+request);
 	$("#wrap").append("wata");
 
-	$("#btnInstall").click(function()
+	alert(request.onsuccess);
+	request.onsuccess = function()
 	{
-		alert(request.onsuccess);
-		request.onsuccess = function()
+		if (request.result)
+			$("#wrap").append("<p>Esta app Ya esta instalada en su dispositivo</p>");
+		else
 		{
-			if (request.result)
-				$("#wrap").append("<p>Esta app Ya esta instalada en su dispositivo</p>");
-			else
-				install(manifestUrl);
+			$("#btnInstall").click(function()
+			{
+				var request = navigator.mozApps.install(manifestUrl);
+				alert(request + "intall " + manifestUrl);
+
+				request.onsuccess = function()
+				{
+					$("#wrap").append("<p>La app se ha instalado en su dispositivo</p>");
+					$("#btnInstall").unbind("click");
+				}
+				request.onerror = function()
+				{
+					$("#wrap").append("<p>Ocurrio un error durante la instalacion: " + this.error.name + "</p>");
+				}
+			});
 		}
-		request.onerror = function()
-		{
-			$("#wrap").append("<p>Ocurrio un error verificando la instalacion: " + this.error.message + "</p>");
-		}
-	});
+	}
+	request.onerror = function()
+	{
+		$("#wrap").append("<p>Ocurrio un error verificando la instalacion: " + this.error.message + "</p>");
+	}
 
 	$("#btnBack").click(function()
 	{
 		history.back();
 	});
 });
-
-function install (manifestUrl) 
-{
-	var request = navigator.mozApps.install(manifestUrl);
-	alert(request + "intall " + manifestUrl);
-
-	request.onsuccess = function()
-	{
-		$("#wrap").append("<p>La app se ha instalado en su dispositivo</p>");
-		$("#btnInstall").unbind("click");
-	}
-	request.onerror = function()
-	{
-		$("#wrap").append("<p>Ocurrio un error durante la instalacion: " + this.error.name + "</p>");
-	}
-}
